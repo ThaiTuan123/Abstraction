b3c832af467d6de39fc98a3e70dd80c920a2bc70
ThaiTuan123 <53283080+ThaiTuan123@users.noreply.github.com>
2020-03-31 11:42:48 +0700

    Add project files.

---
+++ Student/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string studentName ;
+        public int studentAge;
+        public double studentGrage;
+
+        public Student(string name,int age,double grage)
+        {
+            studentName = name;
+            studentAge = age;
+            studentGrage = grage;
+        }
+        public void Show()
+        {
+            string gradeStatus = studentGrage > 5.00 ? "Excellent Student" : "Average Student";
+            Console.WriteLine(studentName+"is"+studentAge+"year old."+gradeStatus);
+        }
+    }
+}
